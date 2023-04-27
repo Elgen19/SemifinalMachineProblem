de344ba0585865bc6fe2b83c21d2a88059725cf4
Elgen19 <elgenprestosa@gmail.com>
2023-04-27 16:23:03 +0800

    deleted commented card group.

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -101,60 +101,6 @@ function BasicExample() {
 
             </Container>
         </>
-
-
-
-
-        // <>
-        //     <NavBar />
-
-        //     <div>
-
-
-        //         <Form style={{ position: 'relative', top: '50%', left: '50%', transform: 'translate(-50%, 70%)' }}>
-        //             <h3 className='text-center'>Register</h3>
-
-        //             <Form.Group className="mx-5 d-flex justify-content-center align-items-center" controlId="formBasicUserName">
-        //                 <Form.Label className='p-3'>User Name</Form.Label>
-        //                 <Form.Control style={{ width: '25%' }} type="text" placeholder="Enter user name" />
-        //             </Form.Group>
-
-        //             <Form.Group className="mx-5 d-flex justify-content-center align-items-center" controlId="formBasicEmail">
-        //                 <Form.Label className='p-3'>E-mail</Form.Label>
-        //                 <Form.Control style={{ width: '25%' }} type="email" placeholder="Enter E-mail" />
-        //             </Form.Group>
-
-        //             <Form.Group className="mx-5 d-flex justify-content-center align-items-center" controlId="formBasicPassword1">
-        //                 <Form.Label className='p-4'>Password</Form.Label>
-        //                 <Form.Control style={{ width: '25%' }} type="password" placeholder="Password" />
-        //             </Form.Group>
-
-        //             <Form.Group className="mx-5 d-flex justify-content-center align-items-center" controlId="formBasicPassword2">
-        //                 <Form.Label className='p-4'>Confirm Password</Form.Label>
-        //                 <Form.Control style={{ width: '25%' }} type="password" placeholder="Confirm Password" />
-        //             </Form.Group>
-
-        //             <Form.Group>
-
-        //                 <Button style={{ width: '20%', position: 'relative', top: '50%', left: '51%', transform: 'translate(-50%, 50%)' }} variant="primary" type="submit">
-        //                     Login
-        //                 </Button>
-        //             </Form.Group>
-        //         </Form>
-
-
-
-        //         <Navbar bg="dark" variant="dark" fixed="bottom">
-        //             <Container>
-        //                 <Navbar.Text className='mx-auto text-center'>
-        //                     <h5>MERIDA. MORE BIKE.</h5>
-        //                     <p> &copy; 2023 MERIDA BIKES. All rights reserved.</p>
-        //                 </Navbar.Text>
-        //             </Container>
-        //         </Navbar>
-        //     </div>
-
-        // </>
     );
 }
 
