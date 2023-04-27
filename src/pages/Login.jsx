8b089769f2a510a616db0c36a81c4cdd1bce1bd1
Elgen19 <elgenprestosa@gmail.com>
2023-04-27 16:19:30 +0800

    deleted card group, commented.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,37 +8,7 @@ function BasicExample() {
             <NavBar />
 
             <div>
-                      {/* <CardGroup>
-                <Card>
-                    <Card.Img variant="top" src="https://d2lljesbicak00.cloudfront.net/merida-v2/crud-zoom-img//master/bikes/2022/BIG_TRAIL_700_blublk_MY2022.tif?p3" />
-                    <Card.Body>
-                        <Card.Title className="text-justify">BIG TRAIL 700</Card.Title>
-                        <Card.Text>
-                            29er aluminium hardcore hardtail with FOX 34 Float 140mm fork, Shimano SLX 1 x 12 drivetrain, XT multi-release shifter and a dropper seat post.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-
-                <Card>
-                    <Card.Img variant="top" src="https://d2lljesbicak00.cloudfront.net/merida-v2/crud-zoom-img//master/bikes/2022_Mockups_Assemblings/BIG_TRAIL_600_grnred_MOC22_1.tif?p3" />
-                    <Card.Body>
-                        <Card.Title>BIG TRAIL 600</Card.Title>
-                        <Card.Text className="text-justify">
-                            29er aluminium hardcore hardtail with Marzocchi Z2 140mm fork, Shimano Deore 1 x 12 drivetrain and a dropper seat post.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-
-                <Card>
-                    <Card.Img variant="top" src="https://d2lljesbicak00.cloudfront.net/merida-v2/crud-zoom-img//master/bikes/2022/BIG_TRAIL_500_gldblk_MY2022.tif?p3" />
-                    <Card.Body>
-                        <Card.Title>BIG TRAIL 400</Card.Title>
-                        <Card.Text className="text-justify">
-                            29er aluminium hardcore hardtail with Suntour XCR34 140mm air fork, Shimano Deore 1 x 10 drivetrain and a dropper seat post.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </CardGroup> */}
+                     
                 <Form style={{ position: 'relative', top: '50%', left: '50%', transform: 'translate(-50%, 70%)' }}>
                     <h3 className='text-center'>Sign In</h3>
 
